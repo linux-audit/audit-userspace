4e9dadb1a8b83c34a08596a2dad6317651838bd1
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2011-04-12 17:38:05 +0000

    Audisp-remote should keep a capability if local_port < 1024
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@508 03a675c2-f56d-4096-908f-63dba836b7e4

--- audisp/plugins/remote/audisp-remote.c
+++ audisp/plugins/remote/audisp-remote.c
@@ -438,6 +438,9 @@ int main(int argc, char *argv[])
 #ifdef HAVE_LIBCAP_NG
 	// Drop all capabilities
 	capng_clear(CAPNG_SELECT_BOTH);
+	if (config.local_port < 1024)
+		capng_update(CAPNG_ADD, CAPNG_EFFECTIVE|CAPNG_PERMITTED,
+			CAP_NET_BIND_SERVICE);
 	capng_apply(CAPNG_SELECT_BOTH);
 #endif
 
