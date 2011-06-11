63689be802a4087340304f60748529f870e4c2e3
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2011-06-11 13:45:13 +0000

    Fix off by 1 in remote-fgets
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@544 03a675c2-f56d-4096-908f-63dba836b7e4

--- audisp/plugins/remote/remote-fgets.c
+++ audisp/plugins/remote/remote-fgets.c
@@ -21,6 +21,7 @@
  */
 
 #include "config.h"
+#include <assert.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
@@ -43,28 +44,30 @@ int remote_fgets_eof(void)
 int remote_fgets_more(size_t blen)
 {
 	char *ptr = strchr(buffer, '\n');
-	if (ptr || (size_t)(current-buffer) >= blen)
+	assert(blen != 0);
+	if (ptr || (size_t)(current-buffer) >= blen-1)
 		return 1;
 	return 0;
 }
 
 int remote_fgets(char *buf, size_t blen, int fd)
 {
-	int len = 0, complete = 0;
+	int complete = 0;
 	size_t check;
 	char *ptr = NULL;
 
+	assert(blen != 0);
 	/* See if we have more in the buffer first */
 	if (current != buffer) {
 		ptr = strchr(buffer, '\n');
-		if (ptr)
-			len = current - buffer;
-		else if ((size_t)(current - buffer) >= blen)
-			ptr = current-1; // have more than blen, so point to end
+		if (ptr == NULL && (size_t)(current - buffer) >= blen-1)
+			ptr = current-1; // have enough to fill blen, so point to end
 	}
 
 	/* Otherwise get some new bytes */
 	if (ptr == NULL && current != eptr && !eof) {
+		ssize_t len;
+
 		/* Use current since we may be adding more */
 		do {
 			len = read(fd, current, eptr - current);
@@ -75,6 +78,7 @@ int remote_fgets(char *buf, size_t blen, int fd)
 			eof = 1;
 		else
 			current[len] = 0;
+		current += len;
 
 		/* Start from beginning to see if we have one */
 		ptr = strchr(buffer, '\n');
@@ -85,28 +89,29 @@ int remote_fgets(char *buf, size_t blen, int fd)
 		ptr++; /* Include the newline */
 		check = ptr - buffer;
 		/* Make sure we are within the right size */
-		if (check > blen)
-			check = blen;
+		if (check > blen-1)
+			check = blen-1;
 		complete = 1;
-	} else if (current+len == eptr) {
+	} else if (current == eptr) {
 		/* We are full but no newline */
-		check = blen;
-		len = eptr - buffer;
+		check = blen-1;
 		complete = 1;
-	} else if (current+len >= buffer+blen) {
+	} else if (current >= buffer+blen-1) {
 		/* Not completely full, no newline, but enough to fill buf */
-		len += (current - buffer);
-		check = blen;
+		check = blen-1;
 		complete = 1;
 	}
 	if (complete) {
+		size_t remainder_len;
+
 		/* Move to external buf and terminate it */
 		memmove(buf, buffer, check);
 		buf[check] = 0;
-		if ((size_t)len > check) {
+		remainder_len = current - (buffer + check);
+		if (remainder_len > 0) {
 			/* We have a few leftover bytes to move */
-			memmove(buffer, buffer+check, len-check);
-			current = buffer+(len-check);
+			memmove(buffer, buffer+check, remainder_len);
+			current = buffer+remainder_len;
 			*current = 0;
 		} else {
 			/* Got the whole thing, just reset */
@@ -115,8 +120,7 @@ int remote_fgets(char *buf, size_t blen, int fd)
 		}
 	} else {
 		/* To get here, no newline, not completely full, and less
-		 * than blen. Not sure this is even possible. */
-		current += len;
+		 * than blen-1. */
 		check = 0;
 	}
 	return check;
