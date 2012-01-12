b2e186ebdb5c03354451e8ba587f545aff6e2dc7
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2012-01-12 14:51:21 +0000

    Sync auparse's auditd config parser with auditd's parser
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@612 03a675c2-f56d-4096-908f-63dba836b7e4

--- auparse/auditd-config.c
+++ auparse/auditd-config.c
@@ -159,7 +159,7 @@ int load_config(struct daemon_conf *config, log_test_t lt)
 	}
 
 	/* it's ok, read line by line */
-	f = fdopen(fd, "r");
+	f = fdopen(fd, "rm");
 	if (f == NULL) {
 		audit_msg(LOG_ERR, "Error - fdopen failed (%s)", 
 			strerror(errno));
@@ -259,8 +259,12 @@ static int nv_split(char *buf, struct _pair *nv)
 
 	/* Make sure there's nothing else */
 	ptr = strtok(NULL, " ");
-	if (ptr)
-		return 1;
+	if (ptr) {
+		/* Allow one option, but check that there's not 2 */
+		ptr = strtok(NULL, " ");
+		if (ptr)
+			return 1;
+	}
 
 	/* Everything is OK */
 	return 0;
