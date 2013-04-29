c416ea1c5d1a95fc668b0eed61cfc175e629062b
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2013-04-29 18:28:15 +0000

    Consume less stack sending audit events
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@792 03a675c2-f56d-4096-908f-63dba836b7e4

--- lib/netlink.c
+++ lib/netlink.c
@@ -276,14 +276,14 @@ retry:
 	else if (rc == 0)
 		return -EINVAL; /* This can't happen anymore */
 	else if (rc > 0 && rep.type == NLMSG_ERROR) {
+		int error = rep.error->error;
 		/* Eat the message */
-		struct audit_reply rep2; /* throw away */
-		(void)audit_get_reply(fd, &rep2, GET_REPLY_NONBLOCKING, 0);
+		(void)audit_get_reply(fd, &rep, GET_REPLY_NONBLOCKING, 0);
 
 		/* NLMSG_ERROR can indicate success, only report nonzero */ 
-		if (rep.error->error) {
-			errno = -rep.error->error;
-			return rep.error->error;
+		if (error) {
+			errno = -error;
+			return error;
 		}
 	}
 	return 0;
