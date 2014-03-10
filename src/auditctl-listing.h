03ecd190ef710a812e9ef3831832e58ec339c3ed
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2014-03-10 14:58:45 +0000

    Split out the rule listing code
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@913 03a675c2-f56d-4096-908f-63dba836b7e4

---
+++ src/auditctl-listing.h
@@ -0,0 +1,33 @@
+/*
+* auditctl-listing.h - Header file for ausearch-llist.c
+* Copyright (c) 2014 Red Hat Inc., Durham, North Carolina.
+* All Rights Reserved.
+*
+* This software may be freely redistributed and/or modified under the
+* terms of the GNU General Public License as published by the Free
+* Software Foundation; either version 2, or (at your option) any
+* later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING. If not, write to the
+* Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* Authors:
+*   Steve Grubb <sgrubb@redhat.com>
+*/
+
+#ifndef CTLLISTING_HEADER
+#define CTLLISTING_HEADER
+
+#include "config.h"
+#include "libaudit.h"
+
+int audit_print_reply(struct audit_reply *rep);
+int key_match(struct audit_reply *rep);
+
+#endif
