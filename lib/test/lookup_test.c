521959de714062d03699722d1badc0a8b45641cf
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2014-03-20 00:35:41 +0000

    Fix up tests
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@933 03a675c2-f56d-4096-908f-63dba836b7e4

--- lib/test/lookup_test.c
+++ lib/test/lookup_test.c
@@ -30,8 +30,7 @@
 
 #include "../libaudit.h"
 
-/* Number of looku
- * #include "config.h"ps of random strings */
+/* Number of lookups of random strings */
 #define RAND_ITERATIONS 1000
 
 /* Maximum size of randomly generated strings, including the terminating NUL. */
@@ -362,7 +361,7 @@ test_machinetab(void)
 #define S2I(S) audit_name_to_machine(S)
 	TEST_I2S((t[i].s[0] == 'i' && t[i].s[1] >= '4' && t[i].s[1] <= '6'
 		 && strcmp(t[i].s + 2, "86") == 0) ||
-		(strncmp(t[i].s, "armv", 4) == 0));
+		(strncmp(t[i].s, "arm", 3) == 0));
 	TEST_S2I(-1);
 #undef I2S
 #undef S2I
