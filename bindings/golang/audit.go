931c3bbdfedff2e4e587610e55ce210095bcb230
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2014-07-30 20:44:36 +0000

    Add go language bindings for limited use of libaudit
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@971 03a675c2-f56d-4096-908f-63dba836b7e4

---
+++ bindings/golang/audit.go
@@ -0,0 +1,59 @@
+package audit
+
+/*
+  The audit package is a go bindings to libaudit that only allows for
+  logging audit events.
+
+  Author Steve Grubb <sgrubb@redhat.com>
+
+*/
+
+// #cgo pkg-config: audit
+// #include <libaudit.h>
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <stdio.h>
+import "C"
+import "unsafe"
+
+const (
+	AUDIT_VIRT_CONTROL = 2500
+	AUDIT_VIRT_RESOURCE = 2501
+	AUDIT_VIRT_MACHINE_ID = 2502
+)
+
+func AuditValueNeedsEncoding(str string) (int) {
+	cstr := C.CString(str)
+	defer C.free(unsafe.Pointer(cstr))
+	len := C.strlen(cstr)
+
+	res := C.audit_value_needs_encoding(cstr, C.uint(len))
+	return int(res)
+}
+
+func AuditEncodeNVString(name string, value string) (string) {
+	cname := C.CString(name)
+	cval := C.CString(value)
+
+	cres := C.audit_encode_nv_string(cname, cval, 0)
+
+	C.free(unsafe.Pointer(cname))
+	C.free(unsafe.Pointer(cval))
+	defer C.free(unsafe.Pointer(cres))
+
+	return C.GoString(cres)
+}
+
+func AuditLogUserEvent(event_type int, message string, result int) (int) {
+	fd := C.audit_open()
+	if fd > 0 {
+		cmsg := C.CString(message)
+		res := C.audit_log_user_message(fd, C.int(event_type), cmsg, nil, nil, nil, C.int(result))
+		C.free(unsafe.Pointer(cmsg))
+		C.close(fd)
+		return int(res)
+	}
+	return -1
+}
+
