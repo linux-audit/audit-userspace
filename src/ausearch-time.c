18e3866ca06e760a982f4725ef2341c4857c9cf3
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2016-06-08 16:48:11 +0000

    Fix another DST bug in ausearch time conversion
    git-svn-id: http://svn.fedorahosted.org/svn/audit/trunk@1255 03a675c2-f56d-4096-908f-63dba836b7e4

--- src/ausearch-time.c
+++ src/ausearch-time.c
@@ -264,9 +264,10 @@ int ausearch_time_start(const char *da, const char *ti)
 /* If da == NULL, use current date */
 /* If ti == NULL, then use midnight 00:00:00 */
 	int rc = 0;
-	struct tm d, t;
+	struct tm d;
 	char *ret;
 
+	clear_tm(&d);
 	if (da == NULL)
 		set_tm_now(&d);
 	else {
@@ -283,6 +284,8 @@ int ausearch_time_start(const char *da, const char *ti)
 					"Error parsing start date (%s)\n", da);
 				return 1;
 			}
+			// FIX DST flag
+			start_time = mktime(&d);
 		} else {
 			int keyword=lookup_time(da);
 			if (keyword == T_RECENT || keyword == T_NOW) {
@@ -301,7 +304,7 @@ int ausearch_time_start(const char *da, const char *ti)
 			tmp_t[0]=0;
 			strncat(tmp_t, ti, sizeof(tmp_t)-1);
 		}
-		ret = strptime(tmp_t, "%X", &t);
+		ret = strptime(tmp_t, "%X", &d);
 		if (ret == NULL) {
 			fprintf(stderr,
 	"Invalid start time (%s). Hour, Minute, and Second are required.\n",
@@ -313,16 +316,15 @@ int ausearch_time_start(const char *da, const char *ti)
 			return 1;
 		}
 	} else
-		clear_tm(&t);
+		clear_tm(&d);
 
-	add_tm(&d, &t);
 	if (d.tm_year < 104) {
 		fprintf(stderr, "Error - year is %d\n", d.tm_year+1900);
 		return -1;
 	}
 set_it:
-	// printf("start is: %s\n", ctime(&start_time));
 	start_time = mktime(&d);
+	// printf("start is: %s\n", ctime(&start_time));
 	if (start_time == -1) {
 		fprintf(stderr, "Error converting start time\n");
 		rc = -1;
@@ -335,9 +337,10 @@ int ausearch_time_end(const char *da, const char *ti)
 /* If date == NULL, use current date */
 /* If ti == NULL, use current time */
 	int rc = 0;
-	struct tm d, t;
+	struct tm d;
 	char *ret;
 
+	clear_tm(&d);
 	if (da == NULL)
 		set_tm_now(&d);
 	else {
@@ -354,6 +357,8 @@ int ausearch_time_end(const char *da, const char *ti)
 					"Error parsing end date (%s)\n", da);
 				return 1;
 			}
+			// FIX DST flag
+			end_time = mktime(&d);
 		} else {
 			int keyword=lookup_time(da);
 			if (keyword == T_RECENT || keyword == T_NOW) {
@@ -378,7 +383,7 @@ int ausearch_time_end(const char *da, const char *ti)
 			tmp_t[0]=0;
 			strncat(tmp_t, ti, sizeof(tmp_t)-1);
 		}
-		ret = strptime(tmp_t, "%X", &t);
+		ret = strptime(tmp_t, "%X", &d);
 		if (ret == NULL) {
 			fprintf(stderr,
 	     "Invalid end time (%s). Hour, Minute, and Second are required.\n",
@@ -392,21 +397,18 @@ int ausearch_time_end(const char *da, const char *ti)
 	} else {
 		time_t tt = time(NULL);
 		struct tm *tv = localtime(&tt);
-		clear_tm(&t);
-		t.tm_hour = tv->tm_hour;
-		t.tm_min = tv->tm_min;
-		t.tm_sec = tv->tm_sec;
-		t.tm_isdst = tv->tm_isdst;
-
+		d.tm_hour = tv->tm_hour;
+		d.tm_min = tv->tm_min;
+		d.tm_sec = tv->tm_sec;
+		d.tm_isdst = tv->tm_isdst;
 	}
-	add_tm(&d, &t);
 	if (d.tm_year < 104) {
 		fprintf(stderr, "Error - year is %d\n", d.tm_year+1900);
 		return -1;
 	}
 set_it:
-	// printf("end is: %s\n", ctime(&end_time));
 	end_time = mktime(&d);
+	// printf("end is: %s\n", ctime(&end_time));
 	if (end_time == -1) {
 		fprintf(stderr, "Error converting end time\n");
 		rc = -1;
