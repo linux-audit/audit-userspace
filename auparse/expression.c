ae37bc40e5f1c5be85343c29c3c4169bb9a6b99d
sgrubb <sgrubb@redhat.com>
2017-03-18 12:45:20 -0400

    In auparse, allow - in field names for ausearch_add_expression()

--- auparse/expression.c
+++ auparse/expression.c
@@ -325,7 +325,7 @@ lex(struct parsing *p)
 			(((C) >= 'a' && (C) <= 'z')	\
 			 || ((C) >= 'A' && (C) <= 'Z')	\
 			 || ((C) >= '0' && (C) <= '9')	\
-			 || (C) == '_')
+			 || (C) == '_' || (C) == '-')
 		if (IS_UNQUOTED_STRING_CHAR(*p->src)) {
 			size_t len;
 
