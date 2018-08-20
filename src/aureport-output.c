00bb9c2301f0484dfb52ce3dcd123394d9e2f3ce
Steve Grubb <sgrubb@redhat.com>
2018-08-20 17:24:55 -0400

    In aureport, fix segfault in file repor

--- src/aureport-output.c
+++ src/aureport-output.c
@@ -581,13 +581,13 @@ void print_per_event_item(llist *l)
 			slist *s = l->s.filename;
 			slist_first(s);
 			if (s->cnt > 1) {
-				char *key = s->cur->key;
+				char *key = s->cur ? s->cur->key : NULL;
 				while (key && strcmp(key, "PARENT") == 0) {
 					slist_next(s);
-					key = s->cur->key;
+					key = s->cur ? s->cur->key : NULL;
 				}
 			}
-			safe_print_string(s->cur->str,0);
+			safe_print_string(s->cur ? s->cur->str : "", 0);
 			printf(" %s %s ",
 				aulookup_syscall(l,buf,sizeof(buf)),
 				aulookup_success(l->s.success));
