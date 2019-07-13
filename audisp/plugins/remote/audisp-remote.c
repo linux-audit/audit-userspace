fa1d539c438508b1d1178a5459c5b98dd3f9b2cd
Steve Grubb <sgrubb@redhat.com>
2019-07-13 11:30:09 -0400

    Add krb5 teardown

--- audisp/plugins/remote/audisp-remote.c
+++ audisp/plugins/remote/audisp-remote.c
@@ -989,7 +989,14 @@ static int negotiate_credentials (void)
 
 static int stop_sock(void)
 {
+	
 	if (sock >= 0) {
+		if (USE_GSS) {
+			OM_uint32 minor_status;
+			gss_delete_sec_context(&minor_status, &my_context,
+						GSS_C_NO_BUFFER);
+			my_context = GSS_C_NO_CONTEXT;
+		}
 		shutdown(sock, SHUT_RDWR);
 		close(sock);
 	}
