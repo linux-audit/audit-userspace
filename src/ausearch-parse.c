c14502e04ef9c19064bfd93ecc80cc936bbaa3b7
Steve Grubb <sgrubb@redhat.com>
2020-01-23 16:24:12 -0500

    ausearch should not process AUDIT_REPLACE event

--- src/ausearch-parse.c
+++ src/ausearch-parse.c
@@ -173,7 +173,7 @@ int extract_search_items(llist *l)
 			case AUDIT_MMAP:
 			case AUDIT_NETFILTER_CFG:
 			case AUDIT_PROCTITLE:
-			case AUDIT_KERN_MODULE...AUDIT_BPF
+			case AUDIT_REPLACE...AUDIT_BPF
 				// Nothing to parse
 				break;
 			case AUDIT_TTY:
