0a8d1f2ce46b77907f7b9e1d0aa5a17735f3d93b
Steve Grubb <sgrubb@redhat.com>
2022-03-10 16:30:34 -0500

    Fix Wdiscarded-qualifiers warning

--- audisp/plugins/zos-remote/zos-remote-plugin.c
+++ audisp/plugins/zos-remote/zos-remote-plugin.c
@@ -53,7 +53,7 @@
  */
 volatile int stop = 0;
 volatile int hup = 0;
-volatile ZOS_REMOTE zos_remote_inst;
+static ZOS_REMOTE zos_remote_inst;
 static plugin_conf_t conf;
 static const char *def_config_file = "/etc/audit/zos-remote.conf";
 static pthread_t submission_thread;
