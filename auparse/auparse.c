4d46a5d42b90d7d61200d8c7de9fb53796080e12
alakatos <alakatos@redhat.com>
2024-07-22 10:16:39 +0200

    Remove extra () from strndupa macro

--- auparse/auparse.c
+++ auparse/auparse.c
@@ -1204,14 +1204,13 @@ static int str2event(char *s, au_event_t *e)
 
 #ifndef HAVE_STRNDUPA
 #define strndupa(s, n)								\
-	(												\
 	({												\
 		const char *__old = (s);					\
 		size_t __len = strnlen (__old, (n));		\
 		char *__new = (char *) alloca(__len + 1);	\
 		__new[__len] = '\0';						\
 		(char *) memcpy (__new, __old, __len);		\
-	}))
+	})
 #endif
 
 /* Returns 0 on success and 1 on error */
