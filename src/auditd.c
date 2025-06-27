a1d441ff24d98169b6a79e6ab5a3f6d07d8976e3
Steve Grubb <ausearch.1@gmail.com>
2025-06-27 11:46:05 -0400

    In auditd, prevent plugin restarts by syncing stop to dispatcher immediately
    Updated the SIGTERM handler in auditd to shut down the dispatcher before
    stopping the event loop

--- src/auditd.c
+++ src/auditd.c
@@ -127,10 +127,11 @@ static void usage(void)
 
 /*
  * SIGTERM handler
- */ 
+ */
 static void term_handler(struct ev_loop *loop, struct ev_signal *sig,
 			int revents)
 {
+	libdisp_shutdown();
 	EV_STOP ();
 }
 
@@ -1122,11 +1123,8 @@ int main(int argc, char *argv[])
 	ev_io_stop (loop, &pipe_watcher);
 	close_pipes();
 
-	// Give DAEMON_END event a little time to be sent in case
-	// of remote logging
+	// Give DAEMON_END event a little time
 	usleep(10000); // 10 milliseconds
-	libdisp_shutdown();
-	usleep(20000); // 20 milliseconds
 
 	// Tear down IO watchers Part 3
 	ev_signal_stop(loop, &sigchld_watcher);
